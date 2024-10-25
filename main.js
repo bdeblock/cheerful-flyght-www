5ad8d5e492470cb6c2ac580578909726a3885c73
bdeblock <bdeblock@users.noreply.github.com>
2024-10-25 10:23:17 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+// js/vue-app.js
+//https://webdeasy.de/en/vue-app-without-build-tool/
+
+const { createApp, onMounted } = Vue;
+const { loadModule } = window['vue3-sfc-loader'];
+const { createVuetify } = Vuetify
+
+const vuetify = createVuetify()
+
+const options = {
+  moduleCache: {
+    vue: Vue,
+  },
+  getFile(url) {
+    return fetch(url).then((resp) =>
+      resp.ok ? resp.text() : Promise.reject(resp)
+    );
+  },
+  addStyle(styleStr) {
+    const style = document.createElement('style');
+    style.textContent = styleStr;
+    const ref = document.head.getElementsByTagName('style')[0] || null;
+    document.head.insertBefore(style, ref);
+  },
+  log(type, ...args) {
+    console.log(type, ...args);
+  },
+};
+
+const app = createApp({
+  setup() {
+    onMounted(() => {
+      const message = 'the component is now mounted.';
+      console.log(message)
+    })
+  },
+  components: {
+    MainApp: Vue.defineAsyncComponent(() =>
+      loadModule('src/components/App.vue', options)
+    ),
+  },
+}).use(vuetify).mount('#app');
