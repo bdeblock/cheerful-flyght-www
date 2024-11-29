2262237072d24638e91b917eb0c4f573be5bda5d
bdeblock <bdeblock@users.noreply.github.com>
2024-11-29 20:29:57 +0100

    Create home.js

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+<script setup>
+  import CoverImage from "./cover-image.vue"
+  import DiscoverUs from "./discover-us.vue"
+  import OurWines from "./our-wines.vue"
+  import Order from "./order.vue"  
+</script>
+
+<template>
+  <cover-image></cover-image>
+  <discover-us></discover-us>
+  <our-wines></our-wines>
+  <order></order>
+</template>
