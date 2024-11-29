a2b063c938e45d4618d5b337249d560b78411388
bdeblock <bdeblock@users.noreply.github.com>
2024-11-29 09:26:27 +0100

    Create home.vue

---
+++ src/components/home.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import CoverImage from "./cover-image.vue"
+  import DiscoverUs from "./discover-us.vue"
+  import OurWines from "./our-wines.vue"
+  import Order from "./order.vue"
+  const { ref, computed } = Vue
+  
+</script>
+
+<template>
+  <cover-image></cover-image>
+  <discover-us></discover-us>
+  <our-wines></our-wines>
+  <order></order>
+</template>
+
